d79b93e01cca816acd049bf8cbd30f61a6716988
bagrusW <ivanovpastet039@gmail.com>
2025-09-28 16:48:12 +0300

    /

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -365,7 +365,7 @@ fun Drugs() {
         border = BorderStroke(1.dp, Color(0xFF000000)),
         modifier = Modifier
             .padding(start = 25.dp, top = 140.dp, end = 0.dp, bottom = 0.dp)
-            .size(height = 45.dp, width = 360.dp)
+            .size(height = 45.dp, width = 365.dp)
             .background(
                 White, shape = RoundedCornerShape(
                     topStart = 16.dp,
@@ -381,15 +381,52 @@ fun Drugs() {
                 contentDescription = "Картинка поиска",
                 modifier = Modifier
                     .padding(horizontal = 22.dp)
-                    .size(25.dp, 45.dp)
+                    .size(20.dp, 45.dp)
             )
             Text(
-                "Поиск препаратов",
+                "Поиск использованных препаратов...",
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 8.dp),
-                fontSize = 20.sp,
-                color = Color(0xFF000000)
+                    .padding(vertical = 14.dp),
+                fontSize = 15.sp,
+                color = Color(0xD2343538)
             )
+        }
+    }
+
+    Card(
+        modifier = Modifier
+            .padding(start = 25.dp, top = 200.dp, end = 0.dp, bottom = 0.dp)
+            .size(height = 130.dp, width = 365.dp)
+            .background(
+                White, shape = RoundedCornerShape(
+                    topStart = 16.dp,
+                    topEnd = 0.dp,
+                    bottomEnd = 0.dp,
+                    bottomStart = 0.dp
+                )
+            )
+    ) {
+        Row {
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.drug),
+                contentDescription = "Картинка препарата",
+                modifier = Modifier
+                    .padding(horizontal = 22.dp, vertical = 25.dp)
+                    .size(30.dp, 55.dp)
+            )
+            Column {
+                Text(
+                    "Бордоская смесь", modifier = Modifier
+                        .padding(vertical = 25.dp),
+                    fontSize = 18.sp,
+                    color = Color(0xFF000000)
+                )
+                Text(
+                    "Защита от грибковых заболеваний",
+                    fontSize = 15.sp,
+                    color = Color(0xFF333030)
+                )
+            }
         }
     }
 }
