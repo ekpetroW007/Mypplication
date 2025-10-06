256d327355d236f9a5d2595714a3ae27a4e20b4a
bagrusW <ivanovpastet039@gmail.com>
2025-10-06 21:42:20 +0300

    занятие 28.09

--- app/src/main/java/com/example/myapplication/viewmodel/ProfileViewmodel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/ProfileViewmodel.kt
@@ -1,9 +1,31 @@
 package com.example.myapplication.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.data.Plant
 
 class ProfileViewmodel : ViewModel() {
-    fun accountExit(newText: String) {
-        // выйти из аккаунта
+    fun addPlant() {
+        Plant(1, "Яблоня", 1, 100, "(-)", 1)
+        // добавление растения
+    }
+
+    fun deletePlant() {
+        // удаление растения
+    }
+
+    fun taskCompleted() {
+        // юзер отмечает задачу сделанной, и она зачеркивается
+    }
+
+    fun showWeekOrMonth() {
+        // юзер выбирает, показывать календарь на месяц или показывать только на неделю в верху экрана
+    }
+
+    fun noTasksScreen() {
+        // юзер выбирает дату, на которую он никаких задач еще не планировал
+    }
+
+    fun screenWithTasks() {
+        // экран, который показывается, когда юзер выбирает дату, на которую он уже запланировал что-то
     }
 }
