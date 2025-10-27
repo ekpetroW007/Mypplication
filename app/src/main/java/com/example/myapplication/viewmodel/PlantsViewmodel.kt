92fefe10da13fae92cbf4d724f19f581d0112fd0
bagrusW <ivanovpastet039@gmail.com>
2025-10-27 16:40:11 +0300

    занятие 28.09

--- app/src/main/java/com/example/myapplication/viewmodel/PlantsViewmodel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/PlantsViewmodel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.data.database.entity.DrugEntity
 import com.example.myapplication.data.database.entity.GardenEntity
 import com.example.myapplication.data.database.entity.PlantEntity
 import com.example.myapplication.data.repository.BookeeperRepository
@@ -21,6 +22,31 @@ class PlantsViewmodel(private val repository: BookeeperRepository) : ViewModel()
             started = SharingStarted.WhileSubscribed(5000L),
             initialValue = emptyList()
         )
+
+    fun addPlant(
+        name: String,
+        period: Int,
+        photo: String,
+        taskId: Int?,
+        drugId: Int?,
+        gardenId: Int
+    ) {
+        viewModelScope.launch {
+            try {
+                val newPlant = PlantEntity(
+                    name = name,
+                    period = period,
+                    photo = photo,
+                    taskId = taskId,
+                    drugId = drugId,
+                    gardenId = gardenId
+                )
+                repository.insertPlant(newPlant)
+            } catch (e: Exception) {
+                Log.d("addPlant", e.toString())
+            }
+        }
+    }
 //    private val _listPlantsByGardenId :
 //        Flow<List<PlantEntity>> = emptyList()
 //    val listPlantsByGardenId = _listPlantsByGardenId.asStateFlow()
