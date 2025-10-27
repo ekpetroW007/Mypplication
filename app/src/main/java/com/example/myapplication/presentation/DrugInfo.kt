bb0f85e2d17497390781335dd5bafeabde5abc36
bagrusW <ivanovpastet039@gmail.com>
2025-10-27 14:56:32 +0300

    занятие 28.09

--- app/src/main/java/com/example/myapplication/presentation/DrugInfo.kt
+++ app/src/main/java/com/example/myapplication/presentation/DrugInfo.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,21 +13,35 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.myapplication.BookeeperApp
 import com.example.myapplication.R
+import com.example.myapplication.presentation.navigation.AppDestinations
+import com.example.myapplication.viewmodel.DrugsViewmodel
+import com.example.myapplication.viewmodel.DrugsViewmodelFactory
 
 @Composable
 fun DrugInfo(navController: NavController) {
+    val application = LocalContext.current.applicationContext as BookeeperApp
+    val viewmodelFactory = DrugsViewmodelFactory(application.repository)
+    val drugsViewmodel: DrugsViewmodel = viewModel(factory = viewmodelFactory)
+    val name = remember { mutableStateOf("") }
+    val purpose = remember { mutableStateOf("") }
+    val consumptionRate = remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .size(600.dp, 100.dp)
@@ -44,6 +59,7 @@ fun DrugInfo(navController: NavController) {
                 modifier = Modifier
                     .padding(start = 22.dp, top = 28.dp)
                     .size(35.dp, 40.dp)
+                    .clickable { navController.popBackStack() }
             )
             Text(
                 "Препарат",
@@ -74,7 +90,7 @@ fun DrugInfo(navController: NavController) {
                         .padding(start = 18.dp, top = 22.dp), fontSize = 22.sp
                 )
                 Text(
-                    "Бордоская смесь",
+                    name.value,
                     fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic,
@@ -102,7 +118,7 @@ fun DrugInfo(navController: NavController) {
                         .padding(start = 18.dp, top = 22.dp), fontSize = 22.sp
                 )
                 Text(
-                    "Защита от грибковых заболеваний",
+                    purpose.value,
                     fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic,
@@ -130,7 +146,7 @@ fun DrugInfo(navController: NavController) {
                         .padding(start = 18.dp, top = 22.dp), fontSize = 22.sp
                 )
                 Text(
-                    "20-30 мг на 10л воды",
+                    consumptionRate.value,
                     fontWeight = FontWeight.Medium,
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic,
