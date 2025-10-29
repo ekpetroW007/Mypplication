caec6bf7a4df9f5ac37bb6dfbbd94ca0a63ec053
bagrusW <ivanovpastet039@gmail.com>
2025-10-29 16:13:51 +0300

    занятие 28.09

--- app/src/main/java/com/example/myapplication/presentation/Calendar.kt
+++ app/src/main/java/com/example/myapplication/presentation/Calendar.kt
@@ -1,19 +1,28 @@
 package com.example.myapplication.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myapplication.presentation.navigation.AppDestinations
 import com.example.myapplication.viewmodel.DrugsViewmodel
@@ -70,5 +79,57 @@ fun DayCard(
     gardensViewmodel: GardensViewmodel,
     id: Int
 ) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = White),
+        modifier = Modifier
+            .padding(top = 40.dp)
+            .size(height = 120.dp, width = 365.dp)
+            .background(
+                color = Color(0xFFFBFCFB), shape = RoundedCornerShape(
+                    topStart = 16.dp,
+                )
+            )
+    ) {
+        Column {
+            Row {
+                Text(
+                    plantName,
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(top = 43.dp, start = 30.dp),
+                    color = Color(0xFF000000),
+                    fontWeight = FontWeight.Medium
+                )
+                Text(
+                    gardenName,
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(top = 43.dp, start = 30.dp),
+                    color = Color(0xFF000000),
+                    fontWeight = FontWeight.Medium
+                )
+            }
+            Text(
+                drugName,
+                fontSize = 25.sp,
+                modifier = Modifier.padding(top = 43.dp, start = 30.dp),
+                color = Color(0xFF000000),
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                taskName,
+                fontSize = 25.sp,
+                modifier = Modifier.padding(top = 43.dp, start = 30.dp),
+                color = Color(0xFF000000),
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                plantName,
+                fontSize = 25.sp,
+                modifier = Modifier.padding(top = 43.dp, start = 30.dp),
+                color = Color(0xFF000000),
+                fontWeight = FontWeight.Medium
+            )
+        }
 
+    }
 }
